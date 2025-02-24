faadd03fe699457b1ce9d9debec1ecbd00a73772
Simon <simon@corpus.io>
2025-02-24 14:01:56 +0100

    fix jar

--- bindings/kotlin/build-jar.gradle.kts
+++ bindings/kotlin/build-jar.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
     id("java-library")
-    id("org.jetbrains.kotlin.jvm") version "1.9.24" // Specify a Kotlin version
+    id("org.jetbrains.kotlin.jvm") version "1.9.24"
     id("maven-publish")
 }
 
-// Define repositories for plugins and dependencies
 repositories {
     mavenCentral()
 }
@@ -15,16 +14,15 @@ version = "1.0.0" // Adjust as needed
 sourceSets {
     main {
         java {
-            srcDirs = listOf("src/main/java", "${projectDir}/generated/java")
+            srcDirs.set(setOf(file("src/main/java"), file("${projectDir}/generated/java")))
         }
         kotlin {
-            srcDirs = listOf("lib/src/main/kotlin")
+            srcDirs.set(setOf(file("lib/src/main/kotlin")))
         }
     }
 }
 
 tasks.jar {
-    // Include native libraries from native-libs/ into the JAR under the native directory
     from("${projectDir}/native-libs") {
         into("native")
     }
