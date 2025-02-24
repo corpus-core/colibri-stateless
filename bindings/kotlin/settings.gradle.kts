f1e0f757f2f6fbacbcdd42341591651478613871
Simon <simon@corpus.io>
2025-02-24 17:42:06 +0100

    upload artifact

--- bindings/kotlin/settings.gradle.kts
+++ bindings/kotlin/settings.gradle.kts
@@ -1,1 +1,13 @@
 rootProject.name = "colibri-jar"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    plugins {
+        id("com.android.library") version "8.1.0"
+        id("org.jetbrains.kotlin.android") version "1.9.24"
+    }
+}
