031d342a703da22b17c6591436bdd4600f7133f8
Simon <simon@corpus.io>
2025-02-27 10:47:43 +0100

    fixed deps

--- test/unittests/verify_only.c
+++ test/unittests/verify_only.c
@@ -1,5 +1,3 @@
-#include "c4_assert.h"
-#include "proofer/eth_req.h"
 #include "unity.h"
 #include "util/bytes.h"
 #include "util/json.h"
@@ -7,12 +5,12 @@
 #include "util/plugin.h"
 #include "util/ssz.h"
 #include "verifier/verify.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 void setUp(void) {
   setenv("C4_STATES_DIR", TESTDATA_DIR "/eth_getLogs1", 1);
-}
-
-void tearDown(void) {
-  // Bereinigung nach jedem Test (falls erforderlich)
 }
 
 void test_verify_only() {
@@ -23,8 +21,6 @@ void test_verify_only() {
   storage.get("proof.ssz", &proof_buf);
 
   c4_verify_from_bytes(&verify_ctx, proof_buf.data, "eth_getLogs", json_parse("[{\"address\":[\"0xdac17f958d2ee523a2206206994597c13d831ec7\"],\"fromBlock\":\"0x14d7970\",\"toBlock\":\"0x14d7970\"}]"), C4_CHAIN_MAINNET);
-
-  //  TEST_ASSERT_TRUE_MESSAGE(verify_ctx.success, verify_ctx.state.error);
   buffer_free(&proof_buf);
 }
 int main(void) {
