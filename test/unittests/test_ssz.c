14f9d82fb7e0994213d103e642413572a99fa720
Simon <simon@corpus.io>
2025-03-01 12:56:30 +0100

    improve

---
+++ test/unittests/test_ssz.c
@@ -0,0 +1,32 @@
+// datei: test_addiere.c
+#include "c4_assert.h"
+#include "unity.h"
+#include "util/bytes.h"
+#include "util/ssz.h"
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void test_ssz() {
+  buffer_t buf  = {0};
+  buffer_t tmp  = {0};
+  bytes_t  data = read_testdata("body.ssz");
+  ssz_ob_t ssz  = ssz_ob(SIGNED_BEACON_BLOCK_CONTAINER, data);
+
+  json_t  json      = json_parse(bprintf(&buf, "%z\n", ssz));
+  json_t  signature = json_get(json, "signature");
+  bytes_t sig       = json_as_bytes(signature, &tmp);
+  ASSERT_HEX_STRING_EQUAL("0xb54bfc2475721ef6377a50017bb94064272a8d9190a055d032c5c4fe28d26c7c4fc5864778df1eebe9b943372e2e52ae068776ce8aec4c1bcf4d9dda5a72fd86e3d13e7b3b5dfe8ce9a59ec91e62f576d9d7ea8bba10c90bd6d5ff6c506fbecc",
+                          sig.data, sig.len, "signature");
+  buffer_free(&tmp);
+  buffer_free(&buf);
+  free(data.data);
+}
+
+int main(void) {
+  UNITY_BEGIN();
+  RUN_TEST(test_ssz);
+  return UNITY_END();
+}
