6afcc1841981f2c6f6249da1cfa66e0bff31b6f2
Simon <simon@corpus.io>
2025-04-04 23:16:20 +0200

    build receipt only

--- src/chains/eth/verifier/eth_verify.c
+++ src/chains/eth/verifier/eth_verify.c
@@ -17,18 +17,30 @@ bool c4_eth_verify(verify_ctx_t* ctx) {
 
   if (ssz_is_type(&ctx->proof, eth_ssz_verification_type(ETH_SSZ_VERIFY_BLOCK_HASH_PROOF)))
     verify_blockhash_proof(ctx);
+#ifdef ETH_TX
   else if (ssz_is_type(&ctx->proof, eth_ssz_verification_type(ETH_SSZ_VERIFY_TRANSACTION_PROOF)))
     verify_tx_proof(ctx);
+#endif
+#ifdef ETH_RECEIPT
   else if (ssz_is_type(&ctx->proof, eth_ssz_verification_type(ETH_SSZ_VERIFY_RECEIPT_PROOF)))
     verify_receipt_proof(ctx);
+#endif
+#ifdef ETH_LOGS
   else if (ssz_is_type(&ctx->proof, eth_ssz_verification_type(ETH_SSZ_VERIFY_LOGS_PROOF)))
     verify_logs_proof(ctx);
+#endif
+#ifdef ETH_ACCOUNT
   else if (ssz_is_type(&ctx->proof, eth_ssz_verification_type(ETH_SSZ_VERIFY_ACCOUNT_PROOF)))
     verify_account_proof(ctx);
+#endif
+#ifdef ETH_CALL
   else if (ssz_is_type(&ctx->proof, eth_ssz_verification_type(ETH_SSZ_VERIFY_CALL_PROOF)))
     verify_call_proof(ctx);
+#endif
+#ifdef ETH_BLOCK
   else if (ssz_is_type(&ctx->proof, eth_ssz_verification_type(ETH_SSZ_VERIFY_BLOCK_PROOF)))
     verify_block_proof(ctx);
+#endif
   else if (ctx->method == NULL && ctx->proof.def->type == SSZ_TYPE_NONE && ctx->sync_data.def->type != SSZ_TYPE_NONE && ctx->data.def->type == SSZ_TYPE_NONE)
     ctx->success = true; // if you only verify the sync data, this is ok
   else {
