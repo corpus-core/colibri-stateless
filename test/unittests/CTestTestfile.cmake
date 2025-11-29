acc9514506344b708edcfc2b38513c8ede93f049
Simon Jentzsch <simon@corpus.io>
2025-11-29 13:51:20 +0100

    cleanup

--- test/unittests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/simon/ws/cc/colibri-stateless/test/unittests
-# Build directory: /Users/simon/ws/cc/colibri-stateless/test/unittests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
