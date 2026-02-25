14b2835c1cd0e6afd1fcaa15862fbdd2fdbdbe08
Steffen Kux <steffen@kuxfamily.de>
2026-02-25 17:37:06 +0100

    added pub.dev relevant info

---
+++ lib/colibri_stateless.dart
@@ -0,0 +1,3 @@
+// Stub for pub.dev repo verification. The published package is built from bindings/dart/.
+// See https://github.com/corpus-core/colibri-stateless/tree/main/bindings/dart
+library colibri_stateless;
