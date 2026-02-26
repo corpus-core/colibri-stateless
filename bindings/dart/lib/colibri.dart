4503981a0bb1a2c8a5df302f09285b895000ecdd
Steffen Kux <steffen@kuxfamily.de>
2026-02-26 12:19:29 +0100

    fix for publish

--- bindings/dart/lib/colibri.dart
+++ bindings/dart/lib/colibri.dart
@@ -2,9 +2,6 @@
 ///
 /// Consider using `package:colibri_stateless/colibri_stateless.dart` for
 /// consistency with the package name and for canonical API documentation.
-///
-/// {@nodoc}
-/// Hidden from dartdoc; use package:colibri_stateless/colibri_stateless.dart for API docs.
 library colibri_stateless.colibri;
 
 export 'src/client.dart';
