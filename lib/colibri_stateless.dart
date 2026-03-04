5906c0cf00d388c15bba997730c042c3bb2dc070
Steffen Kux <steffen@kuxfamily.de>
2026-03-04 13:06:47 +0100

    cleanup repo

--- lib/colibri_stateless.dart
+++
@@ -1,3 +0,0 @@
-// Stub for pub.dev repo verification. The published package is built from bindings/dart/.
-// See https://github.com/corpus-core/colibri-stateless/tree/main/bindings/dart
-library colibri_stateless;
