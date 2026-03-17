f8b84888c0bf3cea41dab188951bf6ab88993d87
Simon Jentzsch <simon@corpus.io>
2026-03-17 13:02:58 +0100

    fix metrics

--- src/chains/eth/server/metrics.c
+++ src/chains/eth/server/metrics.c
@@ -89,7 +89,7 @@ void eth_server_metrics(http_server_t* server, buffer_t* data) {
     size_t   tx_cap     = c4_eth_tx_cache_capacity();
     bprintf(data, "# HELP colibri_eth_tx_cache_entries Number of transaction entries currently in tx cache.\n");
     bprintf(data, "# TYPE colibri_eth_tx_cache_entries gauge\n");
-    bprintf(data, "colibri_eth_tx_cache_entries{chain_id=\"%d\"} %zu\n", (uint32_t) server->chain_id, tx_entries);
+    bprintf(data, "colibri_eth_tx_cache_entries{chain_id=\"%d\"} %l\n", (uint32_t) server->chain_id, (uint64_t)tx_entries);
     bprintf(data, "# HELP colibri_eth_tx_cache_blocks Number of blocks currently in tx cache.\n");
     bprintf(data, "# TYPE colibri_eth_tx_cache_blocks gauge\n");
     bprintf(data, "colibri_eth_tx_cache_blocks{chain_id=\"%d\"} %u\n", (uint32_t) server->chain_id, (unsigned) tx_blocks);
