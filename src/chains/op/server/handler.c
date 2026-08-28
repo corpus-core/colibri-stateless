00c55cf4a1dbbc9d13b1237211273744fc143424
Simon Jentzsch <simon@corpus.io>
2026-08-28 23:21:10 +0200

    fix for windows

--- src/chains/op/server/handler.c
+++ src/chains/op/server/handler.c
@@ -1,19 +1,5 @@
 #include "handler.h"
-#include "../verifier/op_chains_conf.h"
 #include "logger.h"
-#include "op_conf.h"
-#include "util/bytes.h"
-#include <libgen.h>
-#include <limits.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#ifdef __APPLE__
-#include <mach-o/dyld.h>
-#endif
-
-// Timer für verzögerte Initialisierung
-static uv_timer_t delayed_init_timer;
 
 void op_server_init(http_server_t* server) {
   OP_HANDLER_CHECK(server);
